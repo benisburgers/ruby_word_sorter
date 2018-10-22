f074a36af26d00e8f06cb9afefc968ad2a618acb
Benjamin Bar-Gera <benibargera@gmail.com>
2018-10-22 11:03:24 +0200

    Insert function which allows user to enter words to be sorted

--- chapter7_word_array_without_sort.rb
+++ chapter7_word_array_without_sort.rb
@@ -1,43 +1,51 @@
-    words = ["my","favorite","animals", "are", "the", "elephant", "and", "the", "antelope", "and", "the", "favela"]
-    puts words.to_s
-    #Convert all letters into numbers. Output individual words as arrays.
-    ordered_words = words.map(&:chars).map { |letters| letters.map { |letter| letter } }
+words = []
+puts ("Type in as many words as you like to add to the array. Once you're ready, enter an empty line.")
+input = gets.chomp
+words.push input
+while input.length > 0
+  input = gets.chomp
+  words.push input
+end
+words.pop
+puts words.to_s
+#Convert all letters into numbers. Output individual words as arrays.
+ordered_words = words.map(&:chars).map { |letters| letters.map { |letter| letter } }
 
-    i = 1
-    x = 0
-    while i < ordered_words.length
-      if ordered_words[i][x] != nil && ordered_words[i-1][x] != nil
-        if ordered_words[i][x] < ordered_words[i-1][x]
-          ordered_words[i], ordered_words[i-1] = ordered_words[i-1], ordered_words[i]
-          i = 1
-          x = 0
-        else
-          if ordered_words[i][x] == ordered_words[i-1][x]
-            x = x + 1
-          else
-            i = i + 1
-            x = 0
-          end
-        end
+i = 1
+x = 0
+while i < ordered_words.length
+  if ordered_words[i][x] != nil && ordered_words[i-1][x] != nil
+    if ordered_words[i][x] < ordered_words[i-1][x]
+      ordered_words[i], ordered_words[i-1] = ordered_words[i-1], ordered_words[i]
+      i = 1
+      x = 0
+    else
+      if ordered_words[i][x] == ordered_words[i-1][x]
+        x = x + 1
       else
-        if ordered_words[i][x] == nil && ordered_words[i-1][x] == nil
-          i = i + 1
-          x = 0
-        else
-          if ordered_words[i][x] == nil
-            ordered_words[i], ordered_words[i-1] = ordered_words[i-1], ordered_words[i]
-            i = 1
-            x = 0
-          else
-            i = i + 1
-            x = 0
-          end
-        end
+        i = i + 1
+        x = 0
       end
     end
-
-    joined_words = []
-    ordered_words.each do |word|
-      joined_words.push(word.join)
+  else
+    if ordered_words[i][x] == nil && ordered_words[i-1][x] == nil
+      i = i + 1
+      x = 0
+    else
+      if ordered_words[i][x] == nil
+        ordered_words[i], ordered_words[i-1] = ordered_words[i-1], ordered_words[i]
+        i = 1
+        x = 0
+      else
+        i = i + 1
+        x = 0
+      end
     end
-    puts joined_words.to_s
+  end
+end
+
+joined_words = []
+ordered_words.each do |word|
+  joined_words.push(word.join)
+end
+puts joined_words.to_s
