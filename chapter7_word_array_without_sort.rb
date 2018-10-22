d4ae3d605b009e86e8e623cb2e1a16bc2004db98
Benjamin Bar-Gera <benibargera@gmail.com>
2018-10-22 10:42:32 +0200

    Leave output as letters (do not convert to numbers). Create new array joined_words to print ordered words

--- chapter7_word_array_without_sort.rb
+++ chapter7_word_array_without_sort.rb
@@ -1,54 +1,43 @@
-words = ["my","favorite","animals", "are", "the", "elephant", "and", "the", "antelope", "and", "the", "favela"]
+    words = ["my","favorite","animals", "are", "the", "elephant", "and", "the", "antelope", "and", "the", "favela"]
+    puts words.to_s
+    #Convert all letters into numbers. Output individual words as arrays.
+    ordered_words = words.map(&:chars).map { |letters| letters.map { |letter| letter } }
 
-#Convert all letters into numbers. Output individual words as arrays.
-converted_words = words.map(&:chars).map { |letters| letters.map { |letter| letter.to_i 36 } }
-puts converted_words.to_s
-
-i = 1
-x = 0
-while i < converted_words.length
-  if converted_words[i][0] < converted_words[i-1][0]
-    converted_words[i], converted_words[i-1] = converted_words[i-1], converted_words[i]
     i = 1
-  else
-    i = i + 1
-  end
-end
-puts converted_words.to_s
+    x = 0
+    while i < ordered_words.length
+      if ordered_words[i][x] != nil && ordered_words[i-1][x] != nil
+        if ordered_words[i][x] < ordered_words[i-1][x]
+          ordered_words[i], ordered_words[i-1] = ordered_words[i-1], ordered_words[i]
+          i = 1
+          x = 0
+        else
+          if ordered_words[i][x] == ordered_words[i-1][x]
+            x = x + 1
+          else
+            i = i + 1
+            x = 0
+          end
+        end
+      else
+        if ordered_words[i][x] == nil && ordered_words[i-1][x] == nil
+          i = i + 1
+          x = 0
+        else
+          if ordered_words[i][x] == nil
+            ordered_words[i], ordered_words[i-1] = ordered_words[i-1], ordered_words[i]
+            i = 1
+            x = 0
+          else
+            i = i + 1
+            x = 0
+          end
+        end
+      end
+    end
 
-i = 1
-x = 0
-while i < converted_words.length
-  if converted_words[i][x] != nil && converted_words[i-1][x] != nil
-    puts converted_words[i].to_s
-    puts converted_words[i-1].to_s
-    puts
-    if converted_words[i][x] < converted_words[i-1][x]
-      converted_words[i], converted_words[i-1] = converted_words[i-1], converted_words[i]
-      i = 1
-      x = 0
-    else
-      if converted_words[i][x] == converted_words[i-1][x]
-        x = x + 1
-      else
-        i = i + 1
-        x = 0
-      end
+    joined_words = []
+    ordered_words.each do |word|
+      joined_words.push(word.join)
     end
-  else
-    if converted_words[i][x] == nil && converted_words[i-1][x] == nil
-      i = i + 1
-      x = 0
-    else
-      if converted_words[i][x] == nil
-        converted_words[i], converted_words[i-1] = converted_words[i-1], converted_words[i]
-        i = 1
-        x = 0
-      else
-        i = i + 1
-        x = 0
-      end
-    end
-  end
-end
-puts converted_words.to_s
+    puts joined_words.to_s
